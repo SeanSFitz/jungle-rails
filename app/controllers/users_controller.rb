a3a3edc13015f8a49cb79e6e9229f0869882a463
SeanSFitz <sean.s.fitz@gmail.com>
2017-07-07 01:31:55 +0000

    done user auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def show
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:root], notice: 'User created!'
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
+end
